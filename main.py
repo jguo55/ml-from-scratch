a33c06e8ffa5eec64d91727cc63c9f240abc7689
asic <148511131+jguo55@users.noreply.github.com>
2024-12-25 17:33:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,32 +13,50 @@ class ExampleNet(nn.Module):
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+    
+class AdamOptimizer():
+    def __init__(self, params, lr=0.001, beta1=0.9, beta2=0.999, eps=0.00000001):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        #initialize moment vectors for each parameter
+        self.m = {}
+        self.v = {}
+        for name, param in model.named_parameters():
+            self.m[name] = np.zeros_like(param.data)
+            self.v[name] = np.zeros_like(param.data)
+        self.t = 1 #initialize timestep
+
+    def updateParameters(self): #.step() in pytorch
+        #here you're supposed to calculate gradients
+        #conveniently, pytorch does that for you through loss.backward() and stores them, so you can just use those
+
+        for name, param in model.named_parameters():
+            #moment vector calculations (moving averages)
+            mt = self.beta1*self.m[name]-(1-self.beta1)*param.grad
+            vt = self.beta2*self.v[name]-(1-self.beta2)*param.grad**2
+
+            #bias correction (because the vectors were initialized to 0)
+            mhat = mt/(1-self.beta1**self.t)
+            vhat = vt/(1-self.beta2**self.t)
+
+            #update parameters
+            param.data = param.data - self.lr*mhat/(np.sqrt(vhat)+self.eps)
+
+            #update vectors
+            self.m[name] = mt
+            self.v[name] = vt
+
+        self.t += 1
+
+    
+
 
 #lowkey the easiest way we can test if this is working is to just create 2 models with the same parameters and see if the function lines up with the pytorch one
 #perhaps use mnist to test
 
-def initializeAdam():
-    #initialize moment vectors for each parameter
-    m = {}
-    v = {}
-    for name, param in model.named_parameters():
-        m[name] = np.zeros_like(param.data)
-        v[name] = np.zeros_like(param.data)
-    return m, v
-
-def Adam(params, learning_rate=0.001, beta1=0.9, beta2=0.999, eps=0.00000001, weight_decay=0, maximize=False): #amsgrad not implemented
-    m, v = initializeAdam()
-    print(m, v)
-    #calculate gradient
-
-    #moment 1 aka moving averages (m)
-    #moment 2 (v)
-
-    #m bias correction (mhat)
-    #v bias correction (vhat)
-    return 0
-
 model = ExampleNet()
 criterion = nn.CrossEntropyLoss
-Adam(model)
+optimizer = AdamOptimizer(model)
 
