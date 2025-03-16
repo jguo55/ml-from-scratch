ebaf788d01575ff778bb1d28b628ae2daa476fb9
jguo55 <148511131+jguo55@users.noreply.github.com>
2025-03-16 13:52:27 -0700

    Final Cleanup

--- adam.py
+++ adam.py
@@ -3,6 +3,12 @@ import torch.nn as nn
 
 import numpy as np
 
+#from https://arxiv.org/pdf/1412.6980
+
+#from https://pytorch.org/docs/stable/generated/torch.optim.Adam.html
+
+#ngl i might do a google collab port later sometime
+
 class ExampleNet(nn.Module):
     def __init__(self):
         super(ExampleNet, self).__init__()
